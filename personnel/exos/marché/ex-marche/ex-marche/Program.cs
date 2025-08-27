981e1ddf6ff1e0486266f55fb13f80bfd6f9f849
ASETML <alban.segalen@eduvaud.ch>
2025-08-27 11:14:11 +0200

    feat(ex marché): Exercice marché

---
+++ personnel/exos/marché/ex-marche/ex-marche/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ex_marche
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Liste de Stands
+            List<Stand> stands = new List<Stand>();
+
+            //Lecture du CSV et ajout du stand à la liste
+            StreamReader sr = new StreamReader("stands.csv");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] values = line.Split(';');
+                stands.Add(new Stand(int.Parse(values[0]), values[1], values[2], int.Parse(values[3]), values[4], double.Parse(values[5])));
+            }
+            sr.Close();
+
+            //Comptage des stands de pêches
+            int nbStandsPeches = 0;
+            foreach (Stand stand in stands)
+            {
+                if (stand.GetProduit() == "Pêches")
+                {
+                    nbStandsPeches++;
+                }
+            }
+
+            Console.WriteLine($"Il y a {nbStandsPeches} vendeurs de pêches");
+
+            //Trouver le producteur qui a le plus de pastèque
+            string NomProducteur = "";
+            int StandNumero = 0;
+            int NombrePasteques = 0;
+
+            foreach (Stand stand in stands)
+            {
+                if (stand.GetProduit() == "Pastèques" && stand.GetQte() > NombrePasteques)
+                {
+                    NomProducteur = stand.GetProducteur();
+                    StandNumero = stand.GetEmplacement();
+                    NombrePasteques = stand.GetQte();
+                }
+            }
+
+            Console.WriteLine($"C'est {NomProducteur} qui a le plus de pastèques (stand {StandNumero}, {NombrePasteques} pièces)");
+        }
+    }
+}
---
+++ personnel/exos/marché/ex-marche/ex-marche/Stand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_marche
+{
+    internal class Stand
+    {
+        private int Emplacement;
+        private string Producteur;
+        private string Produit;
+        private int Qte;
+        private string Unite;
+        private double PrixUnitaire;
+
+        public Stand(int emplacement, string producteur, string produit, int qte, string unite, double prixUnitaire)
+        {
+            Emplacement = emplacement;
+            Producteur = producteur;
+            Produit = produit;
+            Qte = qte;
+            Unite = unite;
+            PrixUnitaire = prixUnitaire;
+        }
+
+        public string GetProduit()
+        {
+            return this.Produit;
+        }
+
+        public int GetEmplacement()
+        {
+            return this.Emplacement;
+        }
+
+        public string GetProducteur()
+        {
+            return this.Producteur;
+        }
+
+        public int GetQte()
+        {
+            return this.Qte;
+        }
+
+        public override string ToString()
+        {
+            return $"Stand numéro {Emplacement} qui appartient à {Producteur} vend {Qte}{Unite} de {Produit} avec un prix unitaire de {PrixUnitaire}";
+        }
+    }
+}
