c6f48f2b57f9d77d2f226312e675b3ed8964f283
ASETML <alban.segalen@eduvaud.ch>
2025-09-03 12:13:55 +0200

    feat(ex words): Fin partie 2

--- personnel/exos/words/ex-words/ex-words/Program.cs
+++ personnel/exos/words/ex-words/ex-words/Program.cs
@@ -43,7 +43,7 @@ namespace ex_words
             //C
             Console.WriteLine("\n## C");
             string[] wordsC = { "+++++", "<<<<<", ">>>>>", "bonjour", "hello", "@@@@", "vert", "rouge", "bleu", "jaune", "#####", "%%%%%%%" };
-            
+
             Array.ForEach(wordsC.SkipWhile(w => !Regex.IsMatch(w, "[a-zA-Z]")).ToArray(), PrintWord);
             Console.WriteLine("On ne peut pas s'en sortir avec un SkipWhile, car la validation cesse de s'effectuer lorsqu'on trouve un élément à garder");
 
@@ -53,6 +53,36 @@ namespace ex_words
 
             Console.WriteLine("The winner is : " + wordsD.First());
             Console.WriteLine("The looser is : " + wordsD.Last());
+
+            //Partie 2
+            Console.WriteLine("\n### 2");
+
+            // http://www.gymomath.ch/javmath/corona/3OC/Fr%C3%A9quences%20d'apparition.pdf
+            Dictionary<char, double> letterReparition = new Dictionary<char, double> { { 'a', 0.084 }, { 'b', 0.0106 }, { 'c', 0.0303 }, { 'd', 0.0418 }, { 'e', 0.1726 }, { 'f', 0.0112 }, { 'g', 0.0127 }, { 'h', 0.0092 }, { 'i', 0.0734 }, { 'j', 0.0031 }, { 'k', 0.0005 }, { 'l', 0.0601 }, { 'm', 0.0296 }, { 'n', 0.0713 }, { 'o', 0.0526 }, { 'p', 0.0301 }, { 'q', 0.0099 }, { 'r', 0.0655 }, { 's', 0.0808 }, { 't', 0.0707 }, { 'u', 0.0574 }, { 'v', 0.0132 }, { 'w', 0.0004 }, { 'x', 0.0045 }, { 'y', 0.0030 }, { 'z', 0.0012 } };
+
+            Dictionary<char, int> letterInWord = new Dictionary<char, int>();
+
+            char[] word = Console.ReadLine().ToLower().ToCharArray();
+
+            Action<char> CountChars = c =>
+            {
+                if (letterInWord.ContainsKey(c))
+                {
+                    letterInWord[c] += 1;
+                    return;
+                }
+                letterInWord.Add(c, 1);
+            };
+
+            Array.ForEach(word, CountChars);
+
+            double epsilon = 0;
+
+            letterInWord.ToList().ForEach(p => epsilon += letterReparition[p.Key] / letterInWord[p.Key]);
+
+            Console.WriteLine(epsilon);
+
+            Console.ReadLine();
         }
     }
 }
