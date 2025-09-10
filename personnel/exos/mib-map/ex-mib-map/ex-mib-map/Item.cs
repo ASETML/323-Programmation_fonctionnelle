45b4b743f01cf760632798557d38e0512f79fb9f
ASETML <alban.segalen@eduvaud.ch>
2025-09-10 12:14:28 +0200

    feat(mib-map): Partie 1 finie, reste JSON + perf

---
+++ personnel/exos/mib-map/ex-mib-map/ex-mib-map/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_mib_map
+{
+    internal class Item
+    {
+        public string producer;
+        public string cat;
+        public string CA;
+        public Item(string producer, string cat, string CA)
+        {
+            this.producer = producer;
+            this.cat = cat;
+            this.CA = CA;
+        }
+    }
+}
