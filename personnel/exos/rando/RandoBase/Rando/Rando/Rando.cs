6b9c2c031aaa5ce0322d538ac8e09714b9442934
ASETML <alban.segalen@eduvaud.ch>
2025-09-17 12:13:39 +0200

    feat(ex-rando): Début de la couleur

--- personnel/exos/rando/RandoBase/Rando/Rando/Rando.cs
+++ personnel/exos/rando/RandoBase/Rando/Rando/Rando.cs
@@ -33,6 +33,25 @@ namespace Rando
             points.ForEach(point => Trace.WriteLine(point));
             this.CreateGraphics().DrawLines(myPen, points.ToArray());
 
-        } 
+            List<(double, Color)> coloredPoints = trackpoints.Select()
+
+            Color[] gradient = new Color[]
+            {
+                Color.FromArgb(255, 144, 238, 144), // Vert clair
+                Color.FromArgb(255, 162, 216, 128),
+                Color.FromArgb(255, 180, 194, 112),
+                Color.FromArgb(255, 198, 172, 96),
+                Color.FromArgb(255, 216, 150, 80),
+                Color.FromArgb(255, 234, 128, 64),
+                Color.FromArgb(255, 244, 106, 48),
+                Color.FromArgb(255, 248,  84, 36),
+                Color.FromArgb(255, 252,  62, 24),
+                Color.FromArgb(255, 254,  48, 18),
+                Color.FromArgb(255, 255,  32, 12),
+                Color.FromArgb(255, 255,  16,  6),
+                Color.FromArgb(255, 255,   0,  0)  // Rouge vif
+            };
+
+        }
     }
 }
